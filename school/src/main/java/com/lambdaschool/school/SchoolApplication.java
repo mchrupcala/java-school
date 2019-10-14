80e7f02c8041fe677e3ffe33d77a1148aaa7263e
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-10-14 18:11:23 -0400

    All of the exception handlings done.

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -1,15 +1,26 @@
 package com.lambdaschool.school;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+// Allows me to do my own routing...don't do this unless I'm using John's trick
+@EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
 public class SchoolApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(SchoolApplication.class, args);
+
+        ApplicationContext ctx = SpringApplication.run(SchoolApplication.class, args);
+        //will return the bean I spelled out, or nothing. Everything works or my exceptions won't work.
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
